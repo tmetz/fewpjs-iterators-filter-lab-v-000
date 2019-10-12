03e555f8231263b7bd5399d747817ad5e9045d61
Tammy Metz <tammy@tammymetz.com>
2019-10-12 22:15:25 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ function findMatching(drivers, driverString) {
 
 function fuzzyMatch(drivers, driverString) {
   let matched = drivers.filter(n => {
-    return n.match(new RegExp(driverString, 'i'));
+    return n.match(new RegExp('^' + driverString));
   });
   return matched;
 }
