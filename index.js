a478bc98e3008d20f6003d6eea5fd6b50f1d81ea
Tammy Metz <tammy@tammymetz.com>
2019-10-12 22:05:40 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Code your solution here
+function findMatching(drivers, driverString) {
+  let matched = drivers.filter(n => {
+    return n.match("/" + driverString + "/i");
+  });
+  return matched;
+}
